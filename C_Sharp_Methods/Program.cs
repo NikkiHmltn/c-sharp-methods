cca63af48db6e217a84658f9793bfca11fe2b9c9
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 16:59:58 -0800

    made two functions of WriteLines to test setting up functions

--- C_Sharp_Methods/Program.cs
+++ C_Sharp_Methods/Program.cs
@@ -6,7 +6,15 @@ namespace C_Sharp_Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteSomething();
+            WriteSpecific("Hello hello hello");
+        }
+        //access modifier (static) return type method name (parameter1, parameter2)
+        static void WriteSomething() => Console.WriteLine("Hello from the new method!");
+        public static void WriteSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+            Console.Read();
         }
     }
 }
