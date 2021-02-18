25e44d4e3b4dfe2151f64c71678de2c4eec2f3ef
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 17:05:16 -0800

    created an add method

--- C_Sharp_Methods/Program.cs
+++ C_Sharp_Methods/Program.cs
@@ -8,13 +8,19 @@ namespace C_Sharp_Methods
         {
             WriteSomething();
             WriteSpecific("Hello hello hello");
+            Console.WriteLine(Add(15, 31));
+            Console.Read();
         }
         //access modifier (static) return type method name (parameter1, parameter2)
         static void WriteSomething() => Console.WriteLine("Hello from the new method!");
         public static void WriteSpecific(string myText)
         {
             Console.WriteLine(myText);
-            Console.Read();
+            
+        }
+        public static int Add(int param1, int param2)
+        {
+            return param1 + param2;
         }
     }
 }
