5d97d902d70717d6214396223734e37565ec9d36
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-17 17:06:52 -0800

    added mult method

--- C_Sharp_Methods/Program.cs
+++ C_Sharp_Methods/Program.cs
@@ -9,6 +9,8 @@ namespace C_Sharp_Methods
             WriteSomething();
             WriteSpecific("Hello hello hello");
             Console.WriteLine(Add(15, 31));
+            Console.WriteLine(Add(Add(1, 2), Add(3, 4)));
+            Console.WriteLine(Mult(2, 8));
             Console.Read();
         }
         //access modifier (static) return type method name (parameter1, parameter2)
@@ -21,6 +23,10 @@ namespace C_Sharp_Methods
         public static int Add(int param1, int param2)
         {
             return param1 + param2;
+        }
+        public static int Mult(int param1, int param2)
+        {
+            return param1 * param2;
         }
     }
 }
